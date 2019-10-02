38a01752e4416eb64c7f6f46ef735a22839f2c0e
WSRIT009 <vadimtre@gmail.com>
2019-10-02 19:11:40 +0300

    Already shows two playing fields in the panel(up to 40 minutes)

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,7 +3,10 @@ import java.awt.*;
 
 /*10:30 Создание игры «Сапёр» на Java*/
 public class JavaSweeper extends JFrame {
-    private JPanel panel;          //19:15 для додавання панелі
+    private JPanel panel;               //19:15 для додавання панелі
+    private final int COLS = 15;        //23:00 додамо деякі константи, які будуть визначати розмір нашого поля і розмір нашого екрана гри
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;  //23:30 додамо константу - розмір 1-го малюнка
 
     public static void main(String[] args) {
         new JavaSweeper();          //16:15 .setVisible(true) -->
@@ -15,13 +18,24 @@ public class JavaSweeper extends JFrame {
         initFrame();        //16:15
     }
 
-    private void initPanel() {//19:25 метод,який буде панель ініціалізувати
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500, 500));
+    private void initPanel() {  //19:25 метод,який буде панель ініціалізувати
+        //panel = new JPanel();
+        //36:00 для малювання малюнку на нашій формі
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) { //36:40 ця ф-ція буде автоматично викликатись кожен раз коли потрібно намалювати форму
+                super.paintComponent(g);
+                g.drawImage(getImage("bomb"), 0, 0, this);
+                g.drawImage(getImage("num1"), IMAGE_SIZE, 0, this);
+            }
+        };
+        //panel.setPreferredSize(new Dimension(500, 500));
+        //24:00  50 --
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
         add(panel);
     }
 
-    private void initFrame() {//14:35
+    private void initFrame() {  //14:35
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");       //встановлюємо заголовок
@@ -29,4 +43,11 @@ public class JavaSweeper extends JFrame {
         setResizable(false);            //ми не будемо змінювати розміри вікна
         setVisible(true);               //щоб форму було видимо
     }
+
+    private Image getImage(String name) {   //33:00 напишемо ф-цію, яка буде займатись отриманням малюнків
+        String filename = "img/" + name.toLowerCase() + ".png";   //по-перше треба визначити імя файла, що містить ці малюнки
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));   //!!!!з-робити папку image ресурсною (right button --> make .. --> ...)
+        return icon.getImage();
+    }
+
 }
