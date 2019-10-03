c241f7790982e1a69746fc394c6cdb87beb13f2e
WSRIT009 <vadimtre@gmail.com>
2019-10-03 17:16:19 +0300

    12.Перечисление кубиков

---
+++ src/sweeper/Box.java
@@ -0,0 +1,21 @@
+package sweeper;
+
+/*1:40 12v it created */
+public enum Box {
+    //UNDERGROUND
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+   //above
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB
+}
